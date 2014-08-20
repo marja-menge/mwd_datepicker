95612c29ce85f71c4fbe3ff97bdd6eb7578a40c2
Marja Menge <info@menge-webdesign.nl>
2014-08-20 15:04:00 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,4 +2,16 @@ $( document ).ready(function() {
   
   $('input[type=date]').mwd_datepicker();
   
+	$('input[type=time]').mwd_timepicker();
+	
+	$('input[type=datetime-local]').mwd_datetimepicker();	
+	
+	$('input[type=datetime]').mwd_datetimepicker({
+		showTimezone: true
+	});		
+	
+	$('input[type=month]').mwd_monthpicker();	
+	
+	$('input[type=week]').mwd_weekpicker();	
+	
 });
